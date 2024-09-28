c4b0255a2fe6e002a09f4c464d51a655d0ceab62
yadnesh-p <puravyadnesh98@gmail.com>
2024-09-28 21:58:36 +0530

    Update TestCases.sh
    Added test cases via script

--- 01_hosting_a_website_on_nginx/TestCases.sh
+++ 01_hosting_a_website_on_nginx/TestCases.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 echo "Hello World"
+a=$(docker ps | wc -l)
+if [ $a -gt 1 ]
+then
+        echo "passed"
+else
+        echo "failed"
+fi
+
+curl http://127.0.0.1:8080 -s > /dev/null
+if [ $? -eq 0 ]
+then
+        echo "passed"
+else
+        echo "failed"
+fi
+
