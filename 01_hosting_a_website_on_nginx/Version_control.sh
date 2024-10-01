51f7fc8642a809e3b2349a22bac5510baa4af830
yadnesh-p <puravyadnesh98@gmail.com>
2024-10-01 16:16:15 +0530

    updated the grep command with more specific search

--- 01_hosting_a_website_on_nginx/Version_control.sh
+++ 01_hosting_a_website_on_nginx/Version_control.sh
@@ -13,9 +13,9 @@ pwd
 cd 01_hosting_a_website_on_nginx
 echo 'Removing Trailing spaces from files if any'
 sed -i 's/\s\+$//g' *.yaml
-previous_version=$(grep -h -m1 'image' *.yaml | cut -d"." -f2 | head -n1)
+previous_version=$(grep -h -m1 'image:' *.yaml | cut -d"." -f2 | head -n1)
 echo "This is the older version of image: $previous_version"
 sed -i s/$previous_version/v$BUILD_NUMBER/g *.yaml
-current_version=$(grep -h 'image' *.yaml | cut -d"." -f2 | head -n1)
+current_version=$(grep -h 'image:' *.yaml | cut -d"." -f2 | head -n1)
 echo "This is the current_version of image: $current_version"
 echo "Version updated"
