e62801293b87b6e9be235540254a8d1b7fd88129
yadnesh-p <puravyadnesh98@gmail.com>
2024-10-01 14:15:12 +0530

    Update Version_control.sh
    Getting to know exact error

--- 01_hosting_a_website_on_nginx/Version_control.sh
+++ 01_hosting_a_website_on_nginx/Version_control.sh
@@ -1,6 +1,7 @@
 #############################################################################
 ## Description: This is a file to check and edit the 
-## version of the yaml files for orchestration purpose
+## version of the yaml files to pull the latest image 
+## from dockerhub
 ##
 ## CREATED BY: Yadnesh Purav
 ## CREATION DATE: 01 OCTOBER 2024
@@ -8,8 +9,9 @@
 
 #!/bin/bash
 echo $BUILD_NUMBER
-previous_version = $(grep -h 'image' 01_hosting_a_website_on_nginx/*.yaml | cut -d"." -f2)
-echo "$previous_version"
+pwd
+#previous_version = $(grep -h 'image' 01_hosting_a_website_on_nginx/*.yaml | cut -d"." -f2)
+#echo "$previous_version"
 #sed -i 's/${previous_version}/v${BUILD_NUMBER}/g' 01_hosting_a_website_on_nginx/*.yaml
 #current_version = $(grep -h 'image' 01_hosting_a_website_on_nginx/*.yaml | cut -d"." -f2)
 #echo "$current_version"
