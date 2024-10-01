66903e437c961b1372edb626892466decd283719
yadnesh-p <puravyadnesh98@gmail.com>
2024-10-01 13:57:53 +0530

    Create Version_control.sh
    Created for k8s versioning to pull the correct image

---
+++ 01_hosting_a_website_on_nginx/Version_control.sh
@@ -0,0 +1,16 @@
+#############################################################################
+## Description: This is a file to check and edit the 
+## version of the yaml files for orchestration purpose
+##
+## CREATED BY: Yadnesh Purav
+## CREATION DATE: 01 OCTOBER 2024
+#############################################################################
+
+#!/bin/bash
+echo $BUILD_NUMBER
+previous_version = $(grep -h 'image' *.yaml | cut -d"." -f2)
+echo "$previous_version"
+#sed -i 's/${previous_version}/v${BUILD_NUMBER}/g' 01_hosting_a_website_on_nginx/*.yaml
+#current_version = $(grep -h 'image' 01_hosting_a_website_on_nginx/*.yaml | cut -d"." -f2)
+#echo "$current_version"
+
